7850fffdce89f89acdbfe7373d6607169667fb70
Hariprasad <hari20185@gmail.com>
2023-02-19 11:50:56 +0530

    Catalogue server script

---
+++ mongodb.sh
@@ -0,0 +1,7 @@
+cp Config/mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+
+systemctl enable mongod
+systemctl start mongod
+
+# update /etc/mongodb.conf file from 127.0.0.1 to 0.0.0.0
